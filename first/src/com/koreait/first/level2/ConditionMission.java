d1da9b552893bbe48eb224541aee90e14690f231
sbsteacher <pirbak@daum.net>
2020-04-29 15:05:15 +0900

    if문 (계절)

--- first/src/com/koreait/first/level2/ConditionMission.java
+++ first/src/com/koreait/first/level2/ConditionMission.java
@@ -1,7 +1,27 @@
 package com.koreait.first.level2;
 
+import java.util.Scanner;
+
 public class ConditionMission {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("월을 입력해 주세요: (1~12)");
+		int season = scan.nextInt();
+		
+		//분기문 에러부터 먼저 잡으세요!!!!
+		if (season <= 0 || season > 12) {
+			System.out.println("잘 못 입력 하셨습니다.");
+		} else if (season <= 2 || season == 12) {
+			System.out.println("겨울");
+		} else if (season <= 5) {
+			System.out.println("봄");
+		} else if (season <= 8) {
+			System.out.println("여름");
+		} else {
+			System.out.println("가을");
+		}
+		
+		//9~11
 		
 	}
 	
