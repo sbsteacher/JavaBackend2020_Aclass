51a52fbc7f8f329b2f1b45feb963c9ba006b66df
sbsteacher <pirbak@daum.net>
2020-05-01 17:29:56 +0900

    printStar2

--- first/src/com/koreait/first/level2/ForMission7.java
+++ first/src/com/koreait/first/level2/ForMission7.java
@@ -3,7 +3,13 @@ package com.koreait.first.level2;
 public class ForMission7 {
 
 	public static void main(String[] args) {
-		int star = 5;
+		int star = 7;
+		for(int i=1; i<=star; i++) {
+			for(int z=0; z<i; z++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
