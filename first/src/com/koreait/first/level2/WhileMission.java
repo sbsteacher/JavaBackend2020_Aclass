14716371fa0589c0b4aa6cc03de791c4d71565e6
sbsteacher <pirbak@daum.net>
2020-05-04 13:47:29 +0900

    while문

--- first/src/com/koreait/first/level2/WhileMission.java
+++ first/src/com/koreait/first/level2/WhileMission.java
@@ -1,9 +1,26 @@
 package com.koreait.first.level2;
 
+import java.util.Scanner;
+
 public class WhileMission {
+	
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String answer = null;
+		int cnt = 0;
 		
+		System.out.print("음악을 재생하시겠습니까?(Y)");
+		answer = scan.next();
+		
+		while("Y".equals(answer)) {
+			System.out.printf("음악을 %d번 재생했습니다.\n", ++cnt);
+			
+			System.out.print("음악을 재생하시겠습니까?(Y)");
+			answer = scan.next();
+		}
+		System.out.println("재생 종료");
 	}
+	
 	/*
 	음악을 재생하시겠습니까?(Y) Y
 	음악을 1번 재생했습니다.
---
+++ first/src/com/koreait/first/level2/WhileMission3.java
@@ -0,0 +1,25 @@
+package com.koreait.first.level2;
+
+import java.util.Scanner;
+
+public class WhileMission3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String answer = null;
+		int cnt = 0;
+		
+		do {
+			System.out.print("음악을 재생하시겠습니까?(Y)");
+			answer = scan.next();
+			
+			if("Y".equals(answer)) {
+				System.out.printf("음악을 %d번 재생했습니다.\n", ++cnt);
+			}
+		} while("Y".equals(answer));
+		System.out.println("재생 종료");
+		scan.close();
+
+	}
+
+}
