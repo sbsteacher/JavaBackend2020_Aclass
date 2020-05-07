320edaa25dac446cb288320f8be6f4191eb61b44
sbsteacher <pirbak@daum.net>
2020-05-07 17:50:32 +0900

    랜덤값 뽑기

---
+++ first/src/com/koreait/first/level2/Random.java
@@ -0,0 +1,20 @@
+package com.koreait.first.level2;
+
+public class Random {
+	public static void main(String[] args) {		
+		//11~30		
+		//int r = (int)(Math.random() * 20 + 11);		
+		
+		//100 ~ 200
+		//int r = (int)(Math.random() * 101 + 100);
+		
+		//15~35
+		//int r = (int)(Math.random() * 21 + 15);
+		
+		//11~30
+		int r = (int)(Math.random() * 20 + 11);
+		
+		
+		
+	}
+}
