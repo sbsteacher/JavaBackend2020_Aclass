4089705bf0c9c4406601801baec5e68534dad538
sbsteacher <pirbak@daum.net>
2020-05-08 16:00:26 +0900

    숫자야구게임

--- first/src/com/koreait/first/level2/Random.java
+++ first/src/com/koreait/first/level2/Random.java
@@ -12,9 +12,47 @@ public class Random {
 		//int r = (int)(Math.random() * 21 + 15);
 		
 		//11~30
-		int r = (int)(Math.random() * 20 + 11);
+		//int r = (int)(Math.random() * 20 + 11);
+		
+		//1~9
 		
 		
-		
+		int[] arr3 = new int[3];		
+		for(int i=0; i<arr3.length; i++) {
+			arr3[i] = (int)(Math.random() * 9 + 1);			
+			for(int z=0; z<i; z++) { //중복체크
+				if(arr3[i] == arr3[z]) {
+					i--; 
+					break;
+				}				
+			}
+		}
+				
+		int[] arr4 = new int[3];
+		int i = 0;
+		parent:
+		while(i<arr4.length) {			
+			arr4[i] = (int)(Math.random() * 9 + 1);			
+			for(int z=0; z<i; z++) { //중복체크
+				if(arr4[i] == arr4[z]) {					
+					continue parent;
+				}				
+			}
+			i++;
+		}	
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ first/src/com/koreait/first/level3/NumberBaseBall.java
@@ -0,0 +1,74 @@
+package com.koreait.first.level3;
+
+import java.util.Scanner;
+
+public class NumberBaseBall {
+
+	public static void main(String[] args) {
+		int gameNo = 3;
+		
+		int[] gameArr = new int[gameNo];
+		int[] myArr = new int[gameNo];
+		
+		//랜덤값 만들기
+		for(int i=0; i<gameArr.length; i++) {
+			gameArr[i] = (int)(Math.random() * 9 + 1);			
+			for(int z=0; z<i; z++) {
+				if(gameArr[i] == gameArr[z]) {
+					i--;
+					break;
+				}
+			}
+		}
+						
+		int strike = 0;
+		int ball = 0;
+		Scanner scan = new Scanner(System.in);
+		while(true) { //or while(strike < gameArr.length) {
+			
+			for(int i=0; i<myArr.length; i++) {
+				System.out.printf("숫자%d 입력: ", i+1);
+				myArr[i] = scan.nextInt();
+			}
+			
+			for(int i=0; i<gameArr.length; i++) {
+				for(int z=0; z<myArr.length; z++) {
+					if(gameArr[i] == myArr[z]) {						
+						if(i == z) {
+							strike++;
+						} else {
+							ball++;
+						}
+					}
+				}
+			}		
+			
+			System.out.printf("S:%d, B:%d, O:%d\n"
+					, strike, ball, gameArr.length - (strike + ball));
+			
+			if(strike == gameArr.length) {				
+				break;
+			}
+			strike = 0;
+			ball = 0;
+		}
+		System.out.println("게임 종료!");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
+
